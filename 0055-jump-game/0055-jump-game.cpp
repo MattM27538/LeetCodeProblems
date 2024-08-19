9000dba472024f625f9914c783e9b5950ca1200e
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2024-08-19 13:34:00 -0700

    Time: 49 ms (35.31%), Space: 51.1 MB (35.01%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -6,6 +6,8 @@ public:
                 break;
             default: {
                 int maxJump=nums[0], posOfMaxJump=0;
+                //Iterate through vector and check if each possible jump
+                // can reach a value that allows for continual jumping forward.
                 for(auto it=begin(nums); it!=end(nums); ++it){
                     switch(maxJump) {case -1: return false;}
                     if(*it>=maxJump) {maxJump=*it;}
