4a9234f7b4f293b5164c885e27dbd6278b43f3fd
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2024-08-25 20:57:39 -0700

    Time: 0 ms (100.00%), Space: 8.2 MB (70.25%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1.0; long long ncopy = n;
+        if(ncopy<0){ncopy=-1*ncopy;}
+        while(ncopy){
+            if(ncopy%2==0){
+                x*=x; 
+                ncopy/=2;
+            } else{
+                ans*=x; 
+                ncopy--;
+                }
+       }
+       if(n<0){ans=1/ans;}
+       return ans;
+    }
+};
