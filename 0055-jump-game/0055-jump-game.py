d59f659efbc3d44e1de14ab9e2369152d0deba28
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2024-09-04 19:44:34 -0700

    Time: 354 ms (59.89%), Space: 17.8 MB (67.40%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -2,7 +2,7 @@ class Solution:
     def canJump(self, nums: List[int]) -> bool:
         maxJump=nums[0]
         #Iterate through vector and check if each possible jump
-        #can reach a value that allows for continual jumping forward.
+        #can reach a value that allows a jump past the end of the previous jump range.
         for x in nums:
             if maxJump<=-1:
                 return False
