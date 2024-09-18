52b74c863aeb1e3d56edd5ac71ccd94af4196d49
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2024-09-17 18:45:57 -0700

    Time: 13 ms (64.62%), Space: 19.6 MB (69.24%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        if(nums.size()==1){return nums[0];}
-        sort(nums.begin(),nums.end());
-        //Check every two pairs of ints. If they do not match return the first int. 
-        if(nums[0]!=nums[1]){return nums[0];}
-        for(int i=2;i<nums.size()-1;i+=2){
-            if(nums[i]!=nums[i+1]){return nums[i];}
-        }
-        return nums[nums.size()-1];
+        int result=0;
+        //Use xor operator to determine what element only appears once.
+        for(int x:nums){result^=x;}
+        return result;
     }
 };
