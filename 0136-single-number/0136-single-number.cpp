d4173f565785b98bec37a3da985dce9b7ce8c5cd
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2024-09-17 18:33:36 -0700

    Time: 19 ms (29.81%), Space: 20.7 MB (24.00%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        if(nums.size()==1){return nums[0];}
+        sort(nums.begin(),nums.end());
+        if(nums[0]!=nums[1]){return nums[0];}
+        for(int i=2;i<nums.size()-1;i+=2){
+            if(nums[i]!=nums[i+1]){return nums[i];}
+        }
+        return nums[nums.size()-1];
+    }
+};
