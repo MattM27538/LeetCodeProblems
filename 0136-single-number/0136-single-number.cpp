c88c989c698bf2b3b2181e1e62754c57129e794d
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2024-09-17 18:35:02 -0700

    Time: 17 ms (37.70%), Space: 20.8 MB (18.13%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,6 +3,7 @@ public:
     int singleNumber(vector<int>& nums) {
         if(nums.size()==1){return nums[0];}
         sort(nums.begin(),nums.end());
+        //Check every two pairs of ints. If they do not match return the first int. 
         if(nums[0]!=nums[1]){return nums[0];}
         for(int i=2;i<nums.size()-1;i+=2){
             if(nums[i]!=nums[i+1]){return nums[i];}
