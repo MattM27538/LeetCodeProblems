15c44d1b7217b453dea090dc2642f485728ae96a
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2024-10-09 19:54:59 -0700

    Time: 0 ms (100.00%), Space: 24.4 MB (71.86%) - LeetHub

--- 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.cpp
+++ 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
         int sol=0;
-        for(int i:nums){if(i<k){++sol;}}
+        for(int i:nums){
+            if(i<k){++sol;}
+        }
         return sol;
     }
 };
