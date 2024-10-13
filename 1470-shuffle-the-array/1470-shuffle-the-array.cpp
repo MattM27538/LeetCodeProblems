36c5cbcc49f90b1f0845f7bfbc3d160041cd07dc
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2024-10-12 19:01:46 -0700

    Time: 5 ms (27.68%), Space: 12.9 MB (5.47%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
         int ncopy=n;
-        //Move elements from second half og graph into final positions.
+        //Move elements from second half of graph into final positions.
         for(int i=1; i<2*n; i+=2){
             nums.insert(nums.begin()+i,nums[ncopy]);
             ncopy+=2;
