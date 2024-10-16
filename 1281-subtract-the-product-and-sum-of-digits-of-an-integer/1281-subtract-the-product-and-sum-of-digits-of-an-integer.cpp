4c1b8fee0cf5aacfe0d44023a258c8bfbec37c70
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2024-10-15 20:16:31 -0700

    Time: 3 ms (33.59%), Space: 7.6 MB (11.88%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.cpp
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int subtractProductAndSum(int n) {
-        string nString=to_string(n);
+        string nAsString=to_string(n);
         int product=1, sum=0;
-        for(char c:nString){product*=c-'0';}
-        for(char c:nString){sum+=c-'0';}
+        for(char c:nAsString){product*=c-'0';}
+        for(char c:nAsString){sum+=c-'0';}
         return product-sum;
     }
 };
