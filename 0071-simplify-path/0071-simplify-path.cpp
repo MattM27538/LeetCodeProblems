b8899177261be8b9c84430972c8ca1b049d85d6b
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-01-09 13:14:01 -0800

    Time: 0 ms (100%), Space: 11 MB (60.16%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -4,20 +4,25 @@ public:
         vector<string> finalPath;
         stringstream pathStream;
         pathStream << path;
-        string dir="";
-        //Read each directory for final path into vector.
-        while(getline(pathStream,dir,'/')){
-            if(dir==".."){
-                if(finalPath.size()>0){finalPath.pop_back();}
+        string dir = "";
+        // Read each directory for final path into vector.
+        while (getline(pathStream, dir, '/')) {
+            if (dir == "..") {
+                if (finalPath.size() > 0) {
+                    finalPath.pop_back();
+                }
+            } else if (dir != "." && dir != "") {
+                finalPath.push_back(dir);
             }
-            else if(dir!="."&& dir!=""){finalPath.push_back(dir);}
         }
-        string ans="/";
-        if(finalPath.size()==0){return ans;}
+        string ans = "/";
+        if (finalPath.size() == 0) {
+            return ans;
+        }
 
-        //Append each directory to string and return it.
-        for(int i=0; i<finalPath.size();++i){
-            ans+=finalPath[i]+"/";
+        // Append each directory to string and return it.
+        for (int i = 0; i < finalPath.size(); ++i) {
+            ans += finalPath[i] + "/";
         }
         ans.pop_back();
         return ans;
