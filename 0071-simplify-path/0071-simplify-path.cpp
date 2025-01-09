cccaefc52f329a52a5a0b4ea20413249e2adda4f
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-01-09 13:13:52 -0800

    Time: 3 ms (60.78%), Space: 10.7 MB (65.89%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -5,23 +5,21 @@ public:
         stringstream pathStream;
         pathStream << path;
         string dir="";
+        //Read each directory for final path into vector.
         while(getline(pathStream,dir,'/')){
-            cout<<dir<<endl;
             if(dir==".."){
                 if(finalPath.size()>0){finalPath.pop_back();}
-                }
-            else if(dir!="."&& !dir.empty()){finalPath.push_back(dir);}
+            }
+            else if(dir!="."&& dir!=""){finalPath.push_back(dir);}
         }
-        // cout<<finalPath.size()<<endl;
         string ans="/";
         if(finalPath.size()==0){return ans;}
+
+        //Append each directory to string and return it.
         for(int i=0; i<finalPath.size();++i){
-            // cout<<finalPath[i]<< " ";
             ans+=finalPath[i]+"/";
-            // ans+="/";
         }
         ans.pop_back();
-        // cout<<ans;
         return ans;
     }
 };
