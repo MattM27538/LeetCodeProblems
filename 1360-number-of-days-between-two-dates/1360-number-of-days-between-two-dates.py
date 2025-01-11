e93213a5dc5eef308c4da2b0ff523a1cdc542e5b
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-01-11 14:50:27 -0800

    Time: 0 ms (100%), Space: 17.8 MB (35.41%) - LeetHub

--- 1360-number-of-days-between-two-dates/1360-number-of-days-between-two-dates.py
+++ 1360-number-of-days-between-two-dates/1360-number-of-days-between-two-dates.py
@@ -7,13 +7,7 @@ class Solution:
         monthTwo=int(date2[5:7])
         dayTwo=int(date2[8:])
 
-        print(yearOne,monthOne,dayOne)
-        print(yearTwo,monthTwo,dayTwo)
-
-        # smallestYear=min(yearOne,yearTwo)
-
-        # daysOne=(yearOne-smallestYear)*365+(12-monthOne)
-        # daysTwo=(yearTwo-smallestYear)*365+(12-monthTwo)
+        #Count up days and months excluding final month of date1.
         daysInOne=yearOne*365
         for i in range(1,monthOne+1):
             daysInMonth=0
@@ -27,8 +21,8 @@ class Solution:
             
             if i==monthOne:
                 daysInOne-=daysInMonth-dayOne
-        print("year one days before leap = ", daysInOne)
         
+        #Count up days and months excluding final month of date2.
         daysInTwo=yearTwo*365
         for i in range(1,monthTwo+1):
             daysInMonth=0
@@ -42,29 +36,22 @@ class Solution:
             
             if i==monthTwo:
                 daysInTwo-=daysInMonth-dayTwo
-        print("year two days before leap = ", daysInTwo)
         
         # Add extra days for leap years.
-        for i in range(1972,2101,4):
-            # print(i)
+        for i in range(1972,max(yearOne,yearTwo)+1,4):
             if yearOne>i:
                 daysInOne+=1
-                # print(i)
             elif yearOne==i: 
-                if (yearOne%100==0 and yearOne%400!=0):
+                if yearOne%100==0 and yearOne%400!=0:
                     pass
                 elif monthOne>=3 or (monthOne==2 and dayOne==29):
                     daysInOne+=1
-                    print("did it", i)
             if yearTwo>i:
                 daysInTwo+=1
             elif yearTwo==i: 
-                if (yearTwo%100==0 and yearTwo%400!=0):
+                if yearTwo%100==0 and yearTwo%400!=0:
                     pass
                 elif monthTwo>=3 or (monthTwo==2 and dayTwo==29):
                     daysInTwo+=1
-
-        print("year one days after leap = ", daysInOne)
-        print("year two days after leap = ", daysInTwo)
 
         return abs(daysInOne-daysInTwo)
