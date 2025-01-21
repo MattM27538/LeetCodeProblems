1f50639448797df002379085df299c371b19012a
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-01-21 12:53:54 -0800

    Time: 0 ms (100%), Space: 312.1 MB (56.54%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -11,23 +11,24 @@
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        if(head->next==nullptr){
+        if (head->next==nullptr) {
             head=nullptr;
             return head;
         }
 
-        ListNode *ptr1=head,*ptr2=head->next;
-        //Loop use two pointers, move ptr2 twice the distance of ptr1. When ptr2 reaches
-        //end of linked-list, ptr1 will be at node before middle node.
-        while(true){
-            if(ptr2->next==nullptr){break;}
-            else if(ptr2->next->next==nullptr){break;}
-            ptr2=ptr2->next->next;
-            ptr1=ptr1->next;
+        ListNode *ptr1=head, *ptr2=head->next;
+
+        // Loop use two pointers, move ptr2 twice the distance of ptr1. When
+        // ptr2 reaches end of linked-list, ptr1 will be at node before middle node.
+        while (true) {
+            if (ptr2->next==nullptr) {break;} 
+            else if (ptr2->next->next==nullptr) {break;}
+            ptr2 = ptr2->next->next;
+            ptr1 = ptr1->next;
         }
 
-        //Delete middle node, point to reattached linked list halves, and return head. 
-        ListNode *temp=ptr1->next;
+        // Delete middle node, reattached linked list halves, and return head.
+        ListNode* temp=ptr1->next;
         ptr1->next=ptr1->next->next;
         delete temp;
         return head;
