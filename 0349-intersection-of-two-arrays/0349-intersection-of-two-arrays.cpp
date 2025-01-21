880dc6c921747c46eafc4ab2450bf891f0d1ccbd
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-01-20 16:34:55 -0800

    Time: 0 ms (100%), Space: 14.2 MB (52.7%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int,char> numMap;
-        set<int> ans;
+        vector<int> ans;
         // Map all values from nums1.
         for(int i=0; i<nums1.size(); ++i){
             numMap[nums1[i]]='t';
@@ -10,9 +10,10 @@ public:
         // Check for same values from nums1 in nums2 and insert them in ans.
         for(int i=0; i<nums2.size(); ++i){
             if(numMap.find(nums2[i])!=numMap.end()){
-                ans.insert(nums2[i]);
+                ans.push_back(nums2[i]);
+                numMap.erase(nums2[i]);
             };
         }
-        return vector<int>(ans.begin(),ans.end());
+        return ans;
     }
 };
