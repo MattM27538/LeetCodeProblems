2980944e821dddfbd920b65739bef2cc971e2427
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-01-23 12:05:11 -0800

    Time: 22 ms (39.38%), Space: 28.5 MB (60.1%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     string removeStars(string s) {
-        int starCount=0;
-        //Read string from end of string. Remove and keep count of stars 
-        //and remove chars for each star encountered. 
-        for(int i=s.length()-1;i>=0;--i){
+        string ans="";
+        //Iterate over string, append chars to new string. If char is a '*'
+        // remove last char from new string.
+        for(int i=0;i<s.length();++i){
             if(s[i]=='*'){
-                ++starCount;
-                s.erase(s.begin()+i);
-                }
-            else if(starCount){
-                s.erase(s.begin()+i);
-                --starCount;
-                }
+                ans.pop_back();
+            } else{
+                ans.push_back(s[i]);
+            }
         }
-        return s;
+        return ans;
     }
 };
