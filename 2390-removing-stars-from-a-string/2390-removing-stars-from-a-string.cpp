1918bfb006d4a72ab47413398b7623eb7cc961f3
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-01-23 12:06:25 -0800

    Time: 16 ms (68.15%), Space: 28.3 MB (80.54%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string removeStars(string s) {
         string ans="";
-        //Iterate over string, append chars to new string. If char is a '*'
+        //Iterate over string, append chars to new string. If char is '*'
         // remove last char from new string.
         for(int i=0;i<s.length();++i){
             if(s[i]=='*'){
