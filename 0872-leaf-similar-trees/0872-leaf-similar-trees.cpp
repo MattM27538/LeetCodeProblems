f681700732276baedcefa1c81be66f5af3976842
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-02-04 13:10:36 -0800

    Time: 0 ms (100%), Space: 15.5 MB (18.54%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -12,10 +12,12 @@
 class Solution {
 public:
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        //Add leaves from each tree to as associated stack.
         addLeavesToStacks(root1,treeNodes,treeLeaves1);
         addLeavesToStacks(root2,treeNodes,treeLeaves2);
 
         if(treeLeaves1.size()!=treeLeaves2.size()){return false;}
+        //Compare leaves in each stack if any don't match return false;
         while(!treeLeaves1.empty()){
             if(treeLeaves1.top()!=treeLeaves2.top()){return false;}
             treeLeaves1.pop();
@@ -24,14 +26,13 @@ public:
         return true;
     }
 
+    //Use dfs to find leaves and add them to a stack associated to their tree.
     void addLeavesToStacks(TreeNode *root, stack<TreeNode*> &treeNodes, stack<int16_t> &leavesStack){
         treeNodes.push(root);
         while(!treeNodes.empty()){
             TreeNode *temp=treeNodes.top();
             treeNodes.pop();
-            if(!temp->right && !temp->left){
-                leavesStack.push(temp->val);
-            }
+            if(!temp->right && !temp->left){leavesStack.push(temp->val);}
             if(temp->right){treeNodes.push(temp->right);}
             if(temp->left){treeNodes.push(temp->left);}
         }
@@ -41,5 +42,4 @@ public:
         stack<TreeNode*> treeNodes;
         stack<int16_t> treeLeaves1;
         stack<int16_t> treeLeaves2;
-
 };
