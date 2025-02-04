6c83b12eada1d2c5b4553c02622476fcffb445b1
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-02-03 21:48:37 -0800

    Time: 0 ms (100%), Space: 35.6 MB (27.46%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
@@ -6,21 +6,22 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* searchBST(TreeNode* root, int &val) {
+    TreeNode* searchBST(TreeNode* root, int& val) {
         /*Traverse tree recursively. If node value is greater that val go left
           otherwise go right. If the direction attempted is null return NULL.
         */
-        if(root->val>val){
-            if(!root->left){return NULL;}
-            return searchBST(root->left,val);
-        } else if (root->val<val) {
-            if(!root->right){return NULL;}
-            return searchBST(root->right,val);
+        if (root->val > val) {
+            if (!root->left) {return NULL;}
+            return searchBST(root->left, val);
+        } else if (root->val < val) {
+            if (!root->right) {return NULL;}
+            return searchBST(root->right, val);
         }
         return root;
     }
