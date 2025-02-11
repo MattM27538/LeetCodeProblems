cefeb609d9529e791860ebfd0d75c89c2bca5d6a
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-02-10 23:22:19 -0800

    Time: 0 ms (100%), Space: 23.2 MB (79.06%) - LeetHub

--- 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
+++ 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
@@ -5,12 +5,9 @@ public:
         for(int num:nums){
             //Increase number of operations by smallest difference between num and multiple
             //of 3 above or below num.
-            if(num%3!=0){
+            if(num%3!=0 || num<3){
                 numOfOperations+=min(((num/3+1)*3)-num,num-(num/3*3));
             }
-            else if(num<3){
-                numOfOperations+=min(num,3-num);
-            } 
         }
         return numOfOperations;
     }
