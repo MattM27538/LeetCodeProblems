a06a10a9b907194ffa1a88d9acfd4129234c1981
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-02-14 21:31:42 -0800

    Time: 3 ms (87.4%), Space: 18.6 MB (77.15%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -4,11 +4,12 @@ class Solution:
         leftSum=0
         rightSum=sum(nums)
         
+        #Compare/update sums of values to the left of index and to right of index.
         for num in nums:
             rightSum-=num
             if leftSum==rightSum:
                 return index
             leftSum+=num
             index+=1
-            
+
         return -1
