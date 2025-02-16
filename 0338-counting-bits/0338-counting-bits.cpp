0b75679b18b50c325b5a376afef2a93e7173adae
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-02-15 21:34:02 -0800

    Time: 2 ms (33.33%), Space: 11.5 MB (8.96%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -4,7 +4,7 @@ public:
         bitset<20> numAsBinary;
         vector<int> ans;
 
-        //Convert i to binary. Push number of appearances of 1 in binary to vector.
+        //Convert i to binary. Push count of 1's in binary to vector.
         for(int i=0;i<=n;++i){
             numAsBinary=i;
             ans.push_back(numAsBinary.count());
