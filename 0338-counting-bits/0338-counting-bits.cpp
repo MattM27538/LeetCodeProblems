cfbe50447f3f7bf976bf79296d267aa99cf6d94f
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-02-15 21:33:00 -0800

    Time: 0 ms (100%), Space: 11.5 MB (8.96%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        bitset<20> binaryNum;
+        bitset<20> numAsBinary;
         vector<int> ans;
 
         //Convert n to binary. Push number of appearances of 1 in binary to vector.
         for(int i=0;i<=n;++i){
-            binaryNum=i;
-            ans.push_back(binaryNum.count());
+            numAsBinary=i;
+            ans.push_back(numAsBinary.count());
         }
 
         return ans;
