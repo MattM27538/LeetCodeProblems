c28787225d24a84f619103c4ad733ea0f5bb9fc5
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-02-21 20:55:56 -0800

    Time: 0 ms (100%), Space: 24.3 MB (6.59%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -2,21 +2,16 @@ class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
         int possiblePlacements={0};
-        int count={0};
         flowerbed.insert(flowerbed.begin(),0);
         flowerbed.push_back(0);
+
+        //Check index plus adjacent plots. If all are empty increase
+        //possible placements by 1.
         for(int i=1;i<flowerbed.size()-1;++i){
-            // cout<<flowerbed[i];
             if(!(flowerbed[i-1]+flowerbed[i]+flowerbed[i+1])){
-                // ++count;
                 ++possiblePlacements;
                 i+=1;
             }
-            // else{
-            //     // possiblePlacements+=count/2;
-            //     cout<<count;
-            //     count=0;
-            //     }
         }
 
         if(possiblePlacements>=n){return true;}
