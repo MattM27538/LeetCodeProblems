b5716aadea7ad5443df0dc5522a636244b0e7742
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-02-21 20:52:57 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int possiblePlacements={0};
+        int count={0};
+        flowerbed.insert(flowerbed.begin(),0);
+        flowerbed.push_back(0);
+        for(int i=1;i<flowerbed.size()-1;++i){
+            // cout<<flowerbed[i];
+            if(!(flowerbed[i-1]+flowerbed[i]+flowerbed[i+1])){
+                // ++count;
+                ++possiblePlacements;
+                i+=2;
+            }
+            // else{
+            //     // possiblePlacements+=count/2;
+            //     cout<<count;
+            //     count=0;
+            //     }
+        }
+
+        if(possiblePlacements>=n){return true;}
+        return false;
+    }
+};
