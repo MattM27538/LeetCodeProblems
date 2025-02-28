ebe9cbdd5b1d96642fe7c1eb9430e5fe4ab4d3b3
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-02-27 22:19:34 -0800

    Time: 3 ms (25.87%), Space: 7.6 MB (91.76%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -2,25 +2,26 @@ class Solution {
 public:
     int minFlips(int a, int b, int c) {
         int minFlips={0};
-        const std::bitset<30> aInBinary{static_cast<unsigned long long>(a)};
-        const std::bitset<30> bInBinary{static_cast<unsigned long long>(b)};
-        const std::bitset<30> cInBinary{static_cast<unsigned long long>(c)};
-        const std::bitset<30> aORbInBinary{aInBinary|bInBinary};
+        const bitset<30> aInBinary{static_cast<unsigned long long>(a)};
+        const bitset<30> bInBinary{static_cast<unsigned long long>(b)};
+        const bitset<30> cInBinary{static_cast<unsigned long long>(c)};
+        const bitset<30> aORbInBinary{aInBinary|bInBinary};
 
+        /*If an index of aORbInBinary does not equal cInBinary then increment 
+          minflips by 1 if aORbInBinary is 0 because only one bit in a or b 
+          need to be flipped to make aOrbInBinary==c==1. Otherwise increment 
+          minflips by the number of 1s in the indices of interest in a and b 
+          which would be flipped to 0's to make aORbInBinary==c==0.
+        */
         for(int i=0;i<aORbInBinary.size();++i){
             if(aORbInBinary[i]==0 && cInBinary[i]==1){
-                std::cout<<i;
                 ++minFlips;
             }
             else if(aORbInBinary[i]==1 && cInBinary[i]==0){
-                std::cout<<i;
-                std::cout<<"else if = "<<aInBinary[i]+bInBinary[i]<<"\n";
                 minFlips+=aInBinary[i]+bInBinary[i];
             }
-            // std::cout<<aORbInBinary[i];
         }
         
-
         return minFlips;
     }
 };
