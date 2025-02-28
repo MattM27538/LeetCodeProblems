f47f0cf6f50db55c03099584729a8d9ae189a9a9
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-02-27 22:27:06 -0800

    Time: 0 ms (100%), Space: 7.7 MB (67.75%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -8,10 +8,10 @@ public:
         const bitset<30> aORbInBinary{aInBinary|bInBinary};
 
         /*If an index of aORbInBinary does not equal cInBinary then increment 
-          minflips by 1 if aORbInBinary is 0 because only one bit in a or b 
-          need to be flipped to make aOrbInBinary==c==1. Otherwise, increment 
-          minflips by the number of 1s in the indices of interest in a and b 
-          which would be flipped to 0's to make aORbInBinary==c==0.
+          minflips by 1 if aORbInBinary[index] is 0 because only one bit in a or b 
+          need to be flipped to make aOrbInBinary[index]==cInBinary[index]==1. 
+          Otherwise, increment minflips by the number of 1s in the indices of interest 
+          in a and b which would be flipped to 0's to make aORbInBinary[index]==c[index]==0.
         */
         for(int i=0;i<aORbInBinary.size();++i){
             if(aORbInBinary[i]==0 && cInBinary[i]==1){
