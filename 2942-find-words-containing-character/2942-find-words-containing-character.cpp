439e939fa5faa3a9892e887fc1703ca7a42e1c46
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-03-02 01:50:31 -0800

    Time: 0 ms (100%), Space: 33 MB (59.65%) - LeetHub

---
+++ 2942-find-words-containing-character/2942-find-words-containing-character.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        std::vector<int> wordIndiciesContainingX={};
+
+        //For each word in words vector check for char x. If found push_back index
+        //of word to final vector and proceed to next word in words vector.
+        for(int i=0;i<words.size();++i){
+            for(char c:words[i]){
+                if(c==x){
+                    wordIndiciesContainingX.push_back(i);
+                    break;
+                }
+            }
+        }
+
+        return wordIndiciesContainingX;
+    }
+};
