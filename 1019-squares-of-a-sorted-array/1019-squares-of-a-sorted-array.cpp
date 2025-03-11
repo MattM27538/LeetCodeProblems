b4d5013205d2324986959718e337968ed9e2f904
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-03-10 21:07:49 -0700

    Time: 12 ms (15.42%), Space: 30.4 MB (38.17%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
@@ -2,30 +2,29 @@ class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         vector<int> ans(nums.size(),0);
-        
-        int *ptr1={&nums[nums.size()-1]};
-        int *ptr2={&nums[0]};
-        int *ptr3={&ans[ans.size()-1]};
+        int *backOfNums={&nums[nums.size()-1]};
+        int *frontOfNums={&nums[0]};
+        int *ansIterator={&ans[ans.size()-1]};
 
         /*Check both end indices of nums for highest absolute value.
           Square that value and push to final vector. Then
           iterate that index left or right towards the center of
           nums until indicies converge.
         */
-        while(ptr1!=ptr2){
-            if(abs(*ptr1)>abs(*ptr2)){
-                *ptr3=pow(*ptr1,2);
-                --ptr1;
+        while(backOfNums!=frontOfNums){
+            if(abs(*backOfNums)>abs(*frontOfNums)){
+                *ansIterator=pow(*backOfNums,2);
+                --backOfNums;
             } else {
-                *ptr3=pow(*ptr2,2);
-                ++ptr2;
+                *ansIterator=pow(*frontOfNums,2);
+                ++frontOfNums;
             }
             
-            --ptr3;
+            --ansIterator;
         }
 
         //Add square of index value at convergence to final vector.
-        *ptr3=pow(*ptr1,2);
+        *ansIterator=pow(*backOfNums,2);
         return ans;
     }
 };
