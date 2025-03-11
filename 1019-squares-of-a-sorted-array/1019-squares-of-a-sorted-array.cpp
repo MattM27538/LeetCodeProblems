384e5ed170bdc538f368c44fae54125f11d98880
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-03-10 21:00:49 -0700

    Time: 3 ms (56.96%), Space: 30.2 MB (85.85%) - LeetHub

---
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> ans(nums.size(),0);
+        int *ptr1={&nums[nums.size()-1]};
+        int *ptr2={&nums[0]};
+        int *ptr3={&ans[ans.size()-1]};
+
+        /*Check both end indices of nums for highest absolute value.
+          Square that value and push to final vector. Then
+          iterate that index left or right towards the center of
+          nums until indicies converge.
+        */
+        while(ptr1!=ptr2){
+            if(abs(*ptr1)>abs(*ptr2)){
+                // ans.insert(ans.begin(),pow(*ptr1,2));
+                *ptr3=pow(*ptr1,2);
+                --ptr1;
+            } else {
+                // ans.insert(ans.begin(),pow(*ptr2,2));
+                *ptr3=pow(*ptr2,2);
+                ++ptr2;
+            }
+            
+            --ptr3;
+        }
+
+        //Add square of index value at convergence to final vector.
+        *ptr3=pow(*ptr1,2);
+        return ans;
+    }
+};
