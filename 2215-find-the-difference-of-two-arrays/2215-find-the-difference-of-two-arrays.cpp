4e402eb79546a95c62dc1c5f3cf7ed321b92ba61
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-03-18 20:41:43 -0700

    Time: 14 ms (89.32%), Space: 35.5 MB (90.12%) - LeetHub

---
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+        std::vector<std::vector<int>> ans={{},{}};
+        std::unordered_map<int,int> mapOfNums1Values;
+
+        // Hash all ints in nums1 key=nums1[i] value=1.
+        for(int i=0;i<nums1.size();++i){
+            mapOfNums1Values[nums1[i]]=1;
+        }
+
+        /* Check if each int in nums2 is in mapOfNums1Values. If int
+           is not a valid key append int to vector of distinct ints  
+           in nums2. Else change key's value to 0.
+        */
+        for(int i=0;i<nums2.size();++i){
+            if(mapOfNums1Values.find(nums2[i])==mapOfNums1Values.end()){
+                ans[1].push_back(nums2[i]);
+                mapOfNums1Values[nums2[i]]=0;
+            }
+            else{
+                mapOfNums1Values[nums2[i]]=0;
+            }
+        }
+
+        /* Iterate through map. For each key(int), if value
+           is 1 append int to vector of distinct ints in nums1.
+        */
+        for(const auto &pair:mapOfNums1Values){
+            if(pair.second==1){
+                ans[0].push_back(pair.first);
+            }
+        }
+        
+        return ans;
+    }
+};
