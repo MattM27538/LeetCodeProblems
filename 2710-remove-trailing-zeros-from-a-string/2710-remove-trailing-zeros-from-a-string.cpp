bd29fd67dc5cf1e3fe946dd50c020fae25effd89
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-04-01 14:05:11 -0700

    Time: 0 ms (100%), Space: 11.4 MB (92.83%) - LeetHub

--- 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string removeTrailingZeros(string num) {
         /*Iterate through string in reverse.
-          Pop all chars that are '0'.
+          Pop all chars that equal '0'.
         */
         while(num[num.length()-1]=='0'){
             num.pop_back();
