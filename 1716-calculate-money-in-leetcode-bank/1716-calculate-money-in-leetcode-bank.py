3c90fd1379c4d6ec1042c94640b8bb486fd8d135
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-04-01 14:30:28 -0700

    Time: 0 ms (100%), Space: 18 MB (19.79%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -1,18 +1,11 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
-        ans=0
-        valToAdd=1
-        offSet=1
-
-        # Add incrementing values to ans increasing
-        # by 1 each addition. If value is divisible by 7
-        # increment starting value.
-        for i in range(1,n+1):
-            ans+=valToAdd
-            if i%7==0:
-                valToAdd=1+offSet
-                offSet+=1
-            else:
-                valToAdd+=1
+        weekCount=n//7
+        remainingDays=n%7
         
-        return ans
+        #Calculate value in bank.
+        total=((weekCount*(weekCount-1))//2)*7 
+        total+=weekCount*28
+        total+=((remainingDays*(remainingDays+1))//2)+(weekCount*remainingDays)
+        
+        return total
