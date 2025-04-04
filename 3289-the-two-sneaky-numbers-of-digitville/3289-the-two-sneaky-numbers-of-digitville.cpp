fe874059637111c75f76a78e160f026c6685cf8e
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-04-04 12:18:42 -0700

    Time: 0 ms (100%), Space: 26.4 MB (30.67%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.cpp
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.cpp
@@ -5,9 +5,9 @@ public:
         ans.reserve(2);
         std::unordered_map<int,bool> allDigits;
 
-        /*Iterate through nums if a num in nums is not in
-          unordered map add it to unordered map, else
-          push num to vector ans.    
+        /*Iterate through vector "nums". If a num in nums 
+          is not in unordered map "allDigits" add it to 
+          allDigits, else push num to vector "ans".    
         */
         for(const int &num:nums){
             if(allDigits.find(num)==allDigits.end()){
