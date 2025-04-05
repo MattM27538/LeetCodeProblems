a71fe5837a45c8e154688fbd3c9d60b310781b26
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-04-04 20:08:30 -0700

    Time: 3 ms (67.84%), Space: 16.8 MB (68.7%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,48 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
-        std::vector<std::vector<int>> ans;
-        std::unordered_map<int,std::vector<int>> groupMembers;
+        std::vector<std::vector<int>> finalGroups;
+        std::unordered_map<int,std::vector<int>> groupSizeToMembersMap;
 
         for(int i={0};i<groupSizes.size();++i){
-            // std::cout<<groupSizes[i]<<" ";
-            if(groupMembers.find(groupSizes[i])==groupMembers.end()){
-                groupMembers[groupSizes[i]]={i};
-                //RESERVE SPACE?
-                // groupMembers[groupSizes[i]].reserve(VALUE HERE -1?);
+            // Add new group to map for size groupSizes[i].
+            if(groupSizeToMembersMap.find(groupSizes[i])==groupSizeToMembersMap.end()){
+                groupSizeToMembersMap[groupSizes[i]]={i};
+                groupSizeToMembersMap[groupSizes[i]].reserve(groupSizes[i]);
             }
             else{
-                groupMembers[groupSizes[i]].push_back(i);
+               groupSizeToMembersMap[groupSizes[i]].push_back(i);
             }
-
-            if(groupMembers[groupSizes[i]].size()==groupSizes[i]){
-                ans.push_back(groupMembers[groupSizes[i]]);
-                groupMembers[groupSizes[i]].clear();
+            // Add filled group to vector of final groups.
+            if(groupSizeToMembersMap[groupSizes[i]].size()==groupSizes[i]){
+                finalGroups.push_back(groupSizeToMembersMap[groupSizes[i]]);
+                groupSizeToMembersMap[groupSizes[i]].clear();
             }
         }
 
-
-        // std::cout<<"\n";
-        // for(const auto &pair:groupMembers){
-        //     // std::cout<<pair.first<<"- ";
-        //     // for(const auto &member:pair.second){
-        //     // std::cout<<member<<" ";
-        //     // }
-
-        //     std::vector<int> group={};
-        //     for(int i={0};i<pair.second.size();++i){
-        //         group.push_back(pair.second[i]);
-        //         if(((i+1)%pair.first)==0){
-        //             ans.push_back(group);
-        //             for(const int &i:group){
-        //                 std::cout<<i;
-        //             }
-        //             std::cout<<"\n";
-        //             group.clear();
-        //         }
-        //     }          
-        // }
-        
-        return ans;
+        return finalGroups;
     }
 };
