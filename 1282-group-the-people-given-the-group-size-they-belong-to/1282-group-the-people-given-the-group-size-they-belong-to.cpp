b526b06cfa98d9e4ef1ab5ae5f6160a2a32df73b
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-04-04 20:07:56 -0700

    Time: 0 ms (100%), Space: 16.7 MB (90.45%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -8,37 +8,40 @@ public:
             // std::cout<<groupSizes[i]<<" ";
             if(groupMembers.find(groupSizes[i])==groupMembers.end()){
                 groupMembers[groupSizes[i]]={i};
+                //RESERVE SPACE?
+                // groupMembers[groupSizes[i]].reserve(VALUE HERE -1?);
             }
             else{
                 groupMembers[groupSizes[i]].push_back(i);
             }
-        }
 
-
-        std::cout<<"\n";
-        for(const auto &pair:groupMembers){
-            // std::cout<<pair.first<<"- ";
-            // for(const auto &member:pair.second){
-            // std::cout<<member<<" ";
-            // }
-
-            std::vector<int> group={};
-            for(int i={0};i<pair.second.size();++i){
-                group.push_back(pair.second[i]);
-                if(((i+1)%pair.first)==0){
-                    ans.push_back(group);
-                    for(const int &i:group){
-                        std::cout<<i;
-                    }
-                    std::cout<<"\n";
-                    group.clear();
-                }
+            if(groupMembers[groupSizes[i]].size()==groupSizes[i]){
+                ans.push_back(groupMembers[groupSizes[i]]);
+                groupMembers[groupSizes[i]].clear();
             }
-            // for(const auto &groupSize:pair.first){
-
-            // }
-            
         }
+
+
+        // std::cout<<"\n";
+        // for(const auto &pair:groupMembers){
+        //     // std::cout<<pair.first<<"- ";
+        //     // for(const auto &member:pair.second){
+        //     // std::cout<<member<<" ";
+        //     // }
+
+        //     std::vector<int> group={};
+        //     for(int i={0};i<pair.second.size();++i){
+        //         group.push_back(pair.second[i]);
+        //         if(((i+1)%pair.first)==0){
+        //             ans.push_back(group);
+        //             for(const int &i:group){
+        //                 std::cout<<i;
+        //             }
+        //             std::cout<<"\n";
+        //             group.clear();
+        //         }
+        //     }          
+        // }
         
         return ans;
     }
