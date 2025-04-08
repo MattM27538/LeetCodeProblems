fc69ad629847acd1cad75fc6dd7f19f101b26873
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-04-07 18:54:52 -0700

    Time: 0 ms (100%), Space: 8.3 MB (20.78%) - LeetHub

--- 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.cpp
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.cpp
@@ -3,6 +3,7 @@ public:
     int countSegments(string s) {
         char prevChar={' '};
         int numOfSegments={0};
+        
         for(const char &charInStr:s){
             if(prevChar==' ' && charInStr!=' '){
                 ++numOfSegments;
