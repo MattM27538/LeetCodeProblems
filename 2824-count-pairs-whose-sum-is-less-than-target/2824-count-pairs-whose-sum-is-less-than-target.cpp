6f3d8c61b5389ff84260b57c5a084fee35f471b4
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-04-08 22:38:35 -0700

    Time: 0 ms (100%), Space: 24.6 MB (50.1%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int countPairs(vector<int>& nums, int target) {
-        int numberOfPairs={0};
+        int numOfPairs={0};
 
         //Iterate through all pairs of indices in nums.
         for(int i={0};i<nums.size();++i){
             for(int j={i+1};j<nums.size();++j){
                 //Check if each pair of indices sums to less than target.
                 if(nums[i]+nums[j]<target){
-                    ++numberOfPairs;
+                    ++numOfPairs;
                 }
             }
         }
 
-        return numberOfPairs;
+        return numOfPairs;
     }
 };
