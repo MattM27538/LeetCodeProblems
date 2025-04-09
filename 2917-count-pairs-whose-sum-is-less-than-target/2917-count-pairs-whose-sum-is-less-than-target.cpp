1fd6c986c12642f321e0a6646a4c110141359509
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-04-08 21:41:55 -0700

    Time: 0 ms (100%), Space: 24.5 MB (85.71%) - LeetHub

--- 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.cpp
+++ 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.cpp
@@ -3,6 +3,7 @@ public:
     int countPairs(vector<int>& nums, int target) {
         int numberOfPairs={0};
         if(nums.size()==1){return numberOfPairs;}
+        
         int *trailingPtr={&nums[0]};
         int *leadingPtr={&nums[1]};
 
@@ -11,7 +12,7 @@ public:
             if((*trailingPtr+*leadingPtr)<target){
                 ++numberOfPairs;
             }
-            // ++trailingPtr;
+
             if((++leadingPtr)==((&nums.back())+1)){
                 ++trailingPtr;
                 leadingPtr=trailingPtr+1;
