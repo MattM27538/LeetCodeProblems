c1cf36dfd043ab434fecb7734e137a4524631e56
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-04-10 20:10:11 -0700

    Time: 0 ms (100%), Space: 21.9 MB (14.58%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int missingNumber={0};
-        //Sum all vals from 1 to n+1 and subtract all vals from nums.
+        //Sum all numbers from 1 to n+1 and subtract all numbers from nums.
         for(int i={0};i<nums.size();++i){
             missingNumber+=i+1;
             missingNumber-=nums[i];
