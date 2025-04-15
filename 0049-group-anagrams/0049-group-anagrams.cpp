0b3d0d96692db8742a06501031d5a194e4f3b8bf
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-04-14 20:28:27 -0700

    Time: 8 ms (96.62%), Space: 25 MB (67.03%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> sortedAnagrams{};
+        unordered_map<string,vector<string>> stringToAnagramMap{}; 
+
+        //Map all string anagrams into vectors. 
+        for(int i{0};i<strs.size();++i){
+            auto temp{strs[i]};
+            sort(temp.begin(),temp.end());
+
+            if(stringToAnagramMap.find(temp)==stringToAnagramMap.end()){
+                stringToAnagramMap[temp]={strs[i]};
+            }
+            else{
+                stringToAnagramMap[temp].push_back(strs[i]);
+            }
+        }
+
+        //Push all anagram vectors to final vector.
+        for(const auto &strAnagramsPair:stringToAnagramMap){
+            sortedAnagrams.push_back(strAnagramsPair.second);
+        }
+        
+        return sortedAnagrams;
+    }
+};
