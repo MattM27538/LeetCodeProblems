d14416e9e6695f9dff705ca40871568e1ea22a5a
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-04-18 12:34:51 -0700

    Time: 3 ms (91.17%), Space: 8.5 MB (94.3%) - LeetHub

--- 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     bool hasSameDigits(string s) {
         while(s.size()>2){
-            std::string tempString{""};
-
             //Perform operations on string s.
             for(int i{0};i<s.size()-1;++i){
-                tempString+=(to_string(((s[i]-'0')+(s[i+1]-'0'))%10));
+                s[i]='0'+(((s[i]-'0')+(s[i+1]-'0'))%10);
             }
 
-            s=tempString;
+            s.pop_back();
         }
 
         if(s[0]==s[1]){
