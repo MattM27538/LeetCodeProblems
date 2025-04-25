2c107fc39322d2929c14ff2234a4bddcea40dd83
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-04-24 22:15:27 -0700

    Time: 0 ms (100%), Space: 8.6 MB (78.2%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     int addDigits(int num) {
-        constexpr auto digitDivisor{10};
-        auto currentSum{0};
-
-        while((num/digitDivisor)>0){
-            //Add all digits to new sum.
-            while((num/digitDivisor)>0){
-                currentSum+=num%digitDivisor;
-                num/=digitDivisor;
-            }
-
-            currentSum+=num;
-            num=currentSum;
-            currentSum=0;
+        if(num==0){
+            return 0;
         }
 
-        return num;
+        if(num%9==0){
+            return 9;
+        }
+
+        return num%9;
     }
 };
