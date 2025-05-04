f0b1fc76f174455893b5ec14a77f80f3be908c01
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-05-04 00:47:30 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root==nullptr){
+            return minimumDepth;
+        }
+
+        queue<TreeNode*> nodesOfTreeLevel{};
+        nodesOfTreeLevel.push(root);
+        auto nodesInLevel{1};
+        ++minimumDepth;
+
+        while(!nodesOfTreeLevel.empty()){
+            //Iterate through each layer of Binary Tree and return minimum depth.
+            for(int i{0};i<nodesInLevel;++i){
+                if(nodesOfTreeLevel.front()->left==nullptr && nodesOfTreeLevel.front()->right==nullptr){
+                    return minimumDepth;
+                }
+                if(nodesOfTreeLevel.front()->left){
+                    nodesOfTreeLevel.push(nodesOfTreeLevel.front()->left);
+                }
+                if{
+                    nodesOfTreeLevel.push(nodesOfTreeLevel.front()->right);
+                }
+
+                nodesOfTreeLevel.pop();
+            }
+
+            nodesInLevel=nodesOfTreeLevel.size();
+            std::cout<<nodesInLevel<<" ";
+            ++minimumDepth;
+        }
+
+        return minimumDepth;
+    }
+
+    int minimumDepth{0};
+};
