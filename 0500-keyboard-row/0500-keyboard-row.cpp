1d535f5e1bab5910dfbb334505c0028f90a1d7c5
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-05-20 09:42:21 -0700

    Time: 0 ms (100%), Space: 9.3 MB (23.27%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool allLettersInSameRow(std::string_view word, int firstLetterRow, std::unordered_map<char,int>& letterToRowMap){
+        for(const auto& letter:word){
+            if(letterToRowMap.at(std::tolower(letter))!=firstLetterRow){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    vector<string> findWords(vector<string>& words) {
+        std::unordered_map<char,int> letterToRowMap{
+            {'q',1},{'w',1},{'e',1},{'r',1},{'t',1},{'y',1},{'u',1},{'i',1},{'o',1},{'p',1},
+            {'a',2},{'s',2},{'d',2},{'f',2},{'g',2},{'h',2},{'j',2},{'k',2},{'l',2},
+            {'z',3},{'x',3},{'c',3},{'v',3},{'b',3},{'n',3},{'m',3},
+        };
+
+        std::vector<std::string> wordsMadeFromOneRow;
+
+        for(const auto& word:words){
+            int firstLetterRow{letterToRowMap.at(std::tolower(word[0]))};
+            if(allLettersInSameRow(word,firstLetterRow,letterToRowMap)){
+                wordsMadeFromOneRow.push_back(word);
+            }
+        }
+
+        return wordsMadeFromOneRow;
+    }
+};
