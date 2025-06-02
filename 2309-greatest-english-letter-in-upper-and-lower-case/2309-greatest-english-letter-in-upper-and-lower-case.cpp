f18fd4ee8c268c2afd99138bd1d0e3635395dc16
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-06-01 17:59:45 -0700

    Time: 5 ms (14.23%), Space: 9.3 MB (13.46%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cpp
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cpp
@@ -4,27 +4,27 @@ public:
         for(const auto& letter:s){
             if((islower(letter) && m_charsInSMap.find(toupper(letter))!=m_charsInSMap.end())
             || (isupper(letter) && m_charsInSMap.find(tolower(letter))!=m_charsInSMap.end())){
-                updateGreatestChar(m_greatestChar,letter);
+                updateGreatestChar(m_greatestCharInS,letter);
             }
 
             m_charsInSMap[letter]=true;
         }
 
-        return m_greatestChar;
+        return m_greatestCharInS;
     }
 
 private: 
-    void updateGreatestChar(std::string& m_greatestChar,const char letter);
+    void updateGreatestChar(std::string& m_greatestCharInS,const char letter);
 
-    std::string m_greatestChar{""};
+    std::string m_greatestCharInS{""};
     std::unordered_map<char,bool> m_charsInSMap{};
 };
 
-void Solution::updateGreatestChar(std::string& m_greatestChar,const char letter){
-    if(m_greatestChar==""){
-        m_greatestChar.push_back(toupper(letter));
+void Solution::updateGreatestChar(std::string& m_greatestCharInS,const char letter){
+    if(m_greatestCharInS==""){
+        m_greatestCharInS.push_back(toupper(letter));
     }
-    else if(toupper(letter)>m_greatestChar[0]){
-        m_greatestChar[0]=toupper(letter);
+    else if(toupper(letter)>m_greatestCharInS[0]){
+        m_greatestCharInS[0]=toupper(letter);
     }
 }
