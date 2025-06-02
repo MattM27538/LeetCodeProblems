8fbb34d1bac99785ab596018bc69def1874a0b95
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-06-01 18:01:26 -0700

    Time: 2 ms (43.08%), Space: 9.3 MB (13.46%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cpp
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cpp
@@ -2,29 +2,29 @@ class Solution {
 public:
     string greatestLetter(string s) {
         for(const auto& letter:s){
-            if((islower(letter) && m_charsInSMap.find(toupper(letter))!=m_charsInSMap.end())
-            || (isupper(letter) && m_charsInSMap.find(tolower(letter))!=m_charsInSMap.end())){
-                updateGreatestChar(m_greatestCharInS,letter);
+            if((islower(letter) && charsInSMap.find(toupper(letter))!=charsInSMap.end())
+            || (isupper(letter) && charsInSMap.find(tolower(letter))!=charsInSMap.end())){
+                updateGreatestChar(greatestChar,letter);
             }
 
-            m_charsInSMap[letter]=true;
+            charsInSMap[letter]=true;
         }
 
-        return m_greatestCharInS;
+        return greatestChar;
     }
 
 private: 
-    void updateGreatestChar(std::string& m_greatestCharInS,const char letter);
+    void updateGreatestChar(std::string& greatestChar,const char letter);
 
-    std::string m_greatestCharInS{""};
-    std::unordered_map<char,bool> m_charsInSMap{};
+    std::string greatestChar{""};
+    std::unordered_map<char,bool> charsInSMap{};
 };
 
-void Solution::updateGreatestChar(std::string& m_greatestCharInS,const char letter){
-    if(m_greatestCharInS==""){
-        m_greatestCharInS.push_back(toupper(letter));
+void Solution::updateGreatestChar(std::string& greatestChar,const char letter){
+    if(greatestChar==""){
+        greatestChar.push_back(toupper(letter));
     }
-    else if(toupper(letter)>m_greatestCharInS[0]){
-        m_greatestCharInS[0]=toupper(letter);
+    else if(toupper(letter)>greatestChar[0]){
+        greatestChar[0]=toupper(letter);
     }
 }
