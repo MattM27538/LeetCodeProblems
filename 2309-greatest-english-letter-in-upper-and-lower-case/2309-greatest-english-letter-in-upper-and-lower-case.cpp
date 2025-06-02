cd37053af78f91df03518d2e9924d8216178a2fc
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-06-01 17:27:23 -0700

    Time: 0 ms (100%), Space: 9.1 MB (29.23%) - LeetHub

---
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string greatestLetter(string s) {
+        for(const auto& letter:s){
+            if(islower(letter)){
+                if(charsInSMap.find(toupper(letter))!=charsInSMap.end()){
+                    updateGreatestChar(greatestChar,letter);
+                }
+            }
+            else{
+                 if(charsInSMap.find(tolower(letter))!=charsInSMap.end()){
+                    updateGreatestChar(greatestChar,letter);
+                 }
+            }
+
+            charsInSMap[letter]=true;
+        }
+
+        return greatestChar;
+    }
+
+private: 
+    void updateGreatestChar(std::string& greatestChar,const char letter);
+
+    std::string greatestChar{""};
+    std::unordered_map<char,bool> charsInSMap{};
+};
+
+void Solution::updateGreatestChar(std::string& greatestChar,const char letter){
+    if(greatestChar==""){
+        greatestChar.push_back(toupper(letter));
+    }
+    else if(toupper(letter)>greatestChar[0]){
+        greatestChar[0]=toupper(letter);
+    }
+}
