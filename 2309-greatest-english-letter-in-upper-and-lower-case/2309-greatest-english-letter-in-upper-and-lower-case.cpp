7087562b86188af5bd21fea9311370ba4eb7b782
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-06-01 18:04:34 -0700

    Time: 7 ms (10.38%), Space: 9.2 MB (23.46%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cpp
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cpp
@@ -2,29 +2,29 @@ class Solution {
 public:
     string greatestLetter(string s) {
         for(const auto& letter:s){
-            if((islower(letter) && m_charsInSMap.find(toupper(letter))!=m_charsInSMap.end())
-            || (isupper(letter) && m_charsInSMap.find(tolower(letter))!=m_charsInSMap.end())){
-                updateGreatestChar(m_greatestCharInS,letter);
+            if((islower(letter) && m_charsInStrMap.find(toupper(letter))!=m_charsInStrMap.end())
+            || (isupper(letter) && m_charsInStrMap.find(tolower(letter))!=m_charsInStrMap.end())){
+                updateGreatestChar(m_greatestCharInStr,letter);
             }
 
-            m_charsInSMap[letter]=true;
+            m_charsInStrMap[letter]=true;
         }
 
-        return m_greatestCharInS;
+        return m_greatestCharInStr;
     }
 
 private: 
-    void updateGreatestChar(std::string& m_greatestCharInS,const char letter);
+    void updateGreatestChar(std::string& m_greatestCharInStr,const char letter);
 
-    std::string m_greatestCharInS{""};
-    std::unordered_map<char,bool> m_charsInSMap{};
+    std::string m_greatestCharInStr{""};
+    std::unordered_map<char,bool> m_charsInStrMap{};
 };
 
-void Solution::updateGreatestChar(std::string& m_greatestCharInS,const char letter){
-    if(m_greatestCharInS==""){
-        m_greatestCharInS.push_back(toupper(letter));
+void Solution::updateGreatestChar(std::string& m_greatestCharInStr,const char letter){
+    if(m_greatestCharInStr==""){
+        m_greatestCharInStr.push_back(toupper(letter));
     }
-    else if(toupper(letter)>m_greatestCharInS[0]){
-        m_greatestCharInS[0]=toupper(letter);
+    else if(toupper(letter)>m_greatestCharInStr[0]){
+        m_greatestCharInStr[0]=toupper(letter);
     }
 }
