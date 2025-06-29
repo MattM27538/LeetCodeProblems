1ac389540ca4816e3bdeaac7caa2f7eef7b5326c
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-06-29 15:47:00 -0700

    Time: 0 ms (100%), Space: 23.8 MB (79.2%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,20 +1,13 @@
-bool stableSortSwap(const int num1,const int num2){
-    //If nums1==0 swap positions with nums 2.
-    if(num1==0){
-        return false;
-    }
-
-    //If nums2==0 don't swap.
-    if(num2==0){
-        return true;
-    }
-
-    return false;
-}
-
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-       std::stable_sort(nums.begin(),nums.end(),stableSortSwap);
+        int leftIndices{0};
+
+        for(decltype(std::ssize(nums)) rightIndices{0}; rightIndices<std::ssize(nums); ++rightIndices){
+            if(nums.data()[rightIndices] != 0){
+                std::swap(nums.data()[rightIndices], nums.data()[leftIndices]);
+                ++leftIndices;
+            }
+        }
     }
 };
