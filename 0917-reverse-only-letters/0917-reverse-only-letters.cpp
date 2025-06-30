69a0371015136237676e01f8600c8ec41be0370a
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-06-29 17:29:58 -0700

    Time: 0 ms (100%), Space: 8 MB (53.41%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        auto forwardIterator{s.begin()};
+        auto reverseIterator{s.end()};
+
+        //Swap locations with characters
+        while(forwardIterator<reverseIterator){
+            if(std::isalpha(*forwardIterator) && std::isalpha(*reverseIterator)){
+                std::swap(*forwardIterator, *reverseIterator);
+                ++forwardIterator;
+                --reverseIterator;
+            }
+            else if(!(std::isalpha(*forwardIterator))){
+                ++forwardIterator;
+            }
+            else if(!(std::isalpha(*reverseIterator))){
+                --reverseIterator;
+            }
+        }
+
+        return s;
+    }
+};
