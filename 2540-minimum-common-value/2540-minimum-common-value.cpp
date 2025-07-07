9a6ec3673032805fcae30c68438ef1d3ae861f0e
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-07-06 17:45:40 -0700

    Time: 3 ms (40.03%), Space: 54.5 MB (81.68%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        //Search for each value from nums2 in nums1.
+        for(const auto& num:nums2){
+            if(std::ranges::binary_search(nums1,num)){
+                return num;
+            }
+        }
+
+        return -1;
+    }
+};
