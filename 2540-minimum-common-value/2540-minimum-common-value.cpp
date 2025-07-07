038f717445953084f9fbfd03d7390d0e0a243d10
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-07-06 18:01:38 -0700

    Time: 43 ms (19.04%), Space: 69.7 MB (12.61%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        //Search for each value from nums2 in nums1.
+        std::unordered_map<int,bool> numsInNums1{};
+
+        //Add all numbers in nums1 to map.
+        for(const auto& num:nums1){
+            numsInNums1[num]=true;
+        }
+
+        //Return the first number in nums2 found in map.
         for(const auto& num:nums2){
-            if(std::ranges::binary_search(nums1,num)){
+            if(numsInNums1.find(num)!=numsInNums1.end()){
                 return num;
             }
         }
