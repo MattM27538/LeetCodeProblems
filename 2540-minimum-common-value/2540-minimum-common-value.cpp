37a8dbd5d67d9f36f4f6dbb5e9c9eef3d79d3f05
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-07-06 17:47:18 -0700

    Time: 3 ms (40.03%), Space: 54.6 MB (57.18%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        //Search for each value from nums2 in nums1.
+        //Search for each value of nums2 in nums1.
         for(const auto& num:nums2){
             if(std::ranges::binary_search(nums1,num)){
                 return num;
