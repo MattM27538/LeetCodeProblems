63209f36db35e6e90635bbd60915047c26126a60
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-07-13 15:26:32 -0700

    Time: 1 ms (16.15%), Space: 8.1 MB (54.74%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.c
@@ -0,0 +1,12 @@
+char findTheDifference(char* s, char* t) {
+    int differenceBetweenStringsAsciiValues=0;
+
+    for(int i=0; i < strlen(t)-1; ++i){
+        differenceBetweenStringsAsciiValues+=t[i];
+        differenceBetweenStringsAsciiValues-=s[i];
+    }
+
+    differenceBetweenStringsAsciiValues+=t[strlen(t)-1];
+
+    return (char) differenceBetweenStringsAsciiValues;
+}
