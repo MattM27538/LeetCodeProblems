989464d01f1091c29f62fa3e5f8844f98a3188db
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-07-16 17:40:40 -0700

    Time: 0 ms (100%), Space: 7.7 MB (87.62%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.c
+++ 0509-fibonacci-number/0509-fibonacci-number.c
@@ -1,12 +1,15 @@
+void calculateNextFibonacciNumbers(int* currentFibonacciNumber, int* nextFibonacciNumber){
+    int temp=*nextFibonacciNumber;
+    *nextFibonacciNumber+=*currentFibonacciNumber;
+    *currentFibonacciNumber=temp;
+}
+
 int fib(int n){
     int currentFibonacciNumber=0;
     int nextFibonacciNumber=1;
 
     for(int i=0; i < n; ++i){
-        //Calculate next two Fibonacci Numbers.
-        int temp=nextFibonacciNumber;
-        nextFibonacciNumber+=currentFibonacciNumber;
-        currentFibonacciNumber=temp;
+        calculateNextFibonacciNumbers(&currentFibonacciNumber, &nextFibonacciNumber);
     }
 
     return currentFibonacciNumber;
