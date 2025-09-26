76c4a9922eb6ae3016182c9e8cd14a8e352c961c
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-09-25 20:11:13 -0700

    Time: 50 ms (32.16%), Space: 34.2 MB (19.01%) - LeetHub

---
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int countConsistentStrings(string allowedCharacters, vector<string>& words) {
+        std::unordered_map<char,bool> mapOfAllowedCharacters {};
+
+        for(auto& character:allowedCharacters){
+            mapOfAllowedCharacters[character] = true;
+        }
+
+        auto numberOfStringsUsingOnlyAllowedCharacters {0};
+
+        for(std::string_view word:words){
+            bool allCharactersInWordAreAllowed {true};
+
+            for(auto& characterInWord:word){
+                if(mapOfAllowedCharacters.find(characterInWord) == mapOfAllowedCharacters.end()){
+                    allCharactersInWordAreAllowed=false;
+                }
+            }
+
+            if(allCharactersInWordAreAllowed){
+                ++numberOfStringsUsingOnlyAllowedCharacters;
+            }
+            
+            allCharactersInWordAreAllowed = true;
+        }
+
+        return numberOfStringsUsingOnlyAllowedCharacters;
+    }
+};
