d59289663295bff44957d0590b01e782cde9ca03
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-10-09 19:42:10 -0700

    Time: 0 ms (100%), Space: 27.3 MB (16.79%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        for(std::size_t i{0};i<s.size()/2;++i){
-            char temp{s[i]};
-            s[i]=s[s.size()-i-1];
-            s[s.size()-i-1]=temp;
+        auto forwardIterator {s.begin()};
+        auto reverseIterator {s.end()-1};
+
+        while(forwardIterator < reverseIterator){
+            char characterFromForwardIterator = *forwardIterator;
+            *forwardIterator = *reverseIterator;
+            *reverseIterator = characterFromForwardIterator;
+
+            ++forwardIterator;
+            --reverseIterator;
         }
     }
 };
