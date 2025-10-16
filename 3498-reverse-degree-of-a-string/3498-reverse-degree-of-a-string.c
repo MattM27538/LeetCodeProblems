6b31d39ec19bce46c7561312c933076d5a6e3374
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-10-15 20:24:31 -0700

    Time: 0 ms (100%), Space: 8.8 MB (1.55%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.c
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.c
@@ -1,13 +1,12 @@
 int reverseDegree(char* s) {
-    int reverseDegree = 0;
-    const int decimalValueForReverseda = 26;
-    const int decimalValueForCharactera = 97;
-    const int offsetFor1IndexedString = 1;
-
-    for(int i = 0; i < strlen(s); ++i){
-        const int decimalValueForCharInString = (int)s[i];
-        reverseDegree += (decimalValueForReverseda - (decimalValueForCharInString - decimalValueForCharactera)) * (i + offsetFor1IndexedString);
-    }
-
-    return reverseDegree;
+    int sum = 0;
+    
+    for (int i = 0; *s; s++)
+        sum += ((26 - *s + 'a') * (++i));
+    
+    /*
+    for (int i = 0; s[i]; i++) 
+        sum += ((26 - s[i] + 'a') * (i + 1));
+    */
+    return sum;
 }
