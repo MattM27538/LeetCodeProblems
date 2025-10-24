5a239f1d13e5a8a8dd23b1e590838f73790d8d37
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-10-23 18:58:14 -0700

    Time: 11 ms (0.17%), Space: 8.4 MB (58.98%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxDepth(string str) {
+        std::stack<char> stackOfCharactersInString {};
+
+        for(auto& character:str){
+            if((character == '(') || (character == ')')){
+                stackOfCharactersInString.push(character);
+            }
+        }
+
+        auto currentNumberOfNestedParenthesis {0};
+        auto maximumNumberOfNestedParenthesis {0};
+
+        while(!(stackOfCharactersInString.empty())){
+            if(stackOfCharactersInString.top() == ')'){
+                ++currentNumberOfNestedParenthesis;
+            } 
+            else{
+                --currentNumberOfNestedParenthesis;
+            }
+            std::cout << stackOfCharactersInString.top() << " " << currentNumberOfNestedParenthesis << "\n";
+            maximumNumberOfNestedParenthesis = max(maximumNumberOfNestedParenthesis, currentNumberOfNestedParenthesis);
+            stackOfCharactersInString.pop();
+        }
+
+        return maximumNumberOfNestedParenthesis;
+    }
+};
