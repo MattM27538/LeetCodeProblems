989bf96bd97e1dbeee2a559a2eab0924883858d3
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-11-06 09:41:01 -0800

    Time: 173 ms (5.91%), Space: 9.1 MB (89.86%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.c
@@ -0,0 +1,34 @@
+bool stringIndexIsSpaceOrNullTerminator(const char* str, const int index){
+    return str[index] == ' ' || str[index] == '\0';
+}
+
+void reverseWordInString(char* str, const int startOfWordIndex, const int endOfWordIndex){
+    char* forwardIteratorOfWord = &(str[startOfWordIndex]);
+    char* reverseIteratorOfWord = &(str[endOfWordIndex]);
+
+    while(forwardIteratorOfWord < reverseIteratorOfWord){
+        const char forwardIteratorChar = *forwardIteratorOfWord;
+        *forwardIteratorOfWord = *reverseIteratorOfWord;
+        *reverseIteratorOfWord = forwardIteratorChar;
+        ++forwardIteratorOfWord;
+        --reverseIteratorOfWord;
+    }
+}
+
+int setStartOfWordIndexToNextWord(const int whitespaceIndex){
+    return whitespaceIndex + 1;
+}
+
+char* reverseWords(char* str) {
+    int startOfWordIndex = 0;
+
+    for(int i = 0; i < strlen(str) + 1; ++i){
+        if(stringIndexIsSpaceOrNullTerminator(str, i)){
+            reverseWordInString(str, startOfWordIndex, i - 1);
+
+            startOfWordIndex = setStartOfWordIndexToNextWord(i);
+        }
+    }
+
+    return str;
+}
