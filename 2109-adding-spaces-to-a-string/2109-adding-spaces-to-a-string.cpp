9235daa1736232fb2e8c2d7f70553f6301a39fc4
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-11-27 18:12:42 -0800

    Time: 23 ms (36.46%), Space: 80.4 MB (100%) - LeetHub

--- 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.cpp
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    string addSpaces(string s, vector<int>& spaces) {
+    string addSpaces(string& s, vector<int>& spaces) {
         std::string stringWithSpacesAdded {""};
         stringWithSpacesAdded.reserve(s.size() + spaces.size());
         int nextSpaceIndexInNewString {0};
