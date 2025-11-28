6a06854694d7a286206504fc089de5fb4ddd1eb8
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-11-27 18:16:06 -0800

    Time: 20 ms (52.99%), Space: 80.5 MB (100%) - LeetHub

--- 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.cpp
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.cpp
@@ -12,7 +12,6 @@ public:
                     ++nextSpaceIndexInNewString;
                 }
             }
-
             stringWithSpacesAdded.push_back(originalString[originalStringIndex]);
         }
 
