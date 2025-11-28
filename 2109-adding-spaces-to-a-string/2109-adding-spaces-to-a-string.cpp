95a38587eae441ff603d2c2861a0859fe62723fa
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2025-11-27 18:07:25 -0800

    Time: 20 ms (52.99%), Space: 85.7 MB (32%) - LeetHub

---
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string addSpaces(string s, vector<int>& spaces) {
+        //Test with reserve
+
+        std::string stringWithSpacesAdded {""};
+        // stringWithSpacesAdded.reserve(s.size() + spaces.size());
+
+        int nextSpaceIndex {0};
+
+        for(decltype(std::ssize(s)) oldStringIndex {0}; oldStringIndex < std::ssize(s); ++oldStringIndex){
+            if(oldStringIndex == spaces[nextSpaceIndex]){
+                stringWithSpacesAdded.push_back(' ');
+                if(nextSpaceIndex < spaces.size()-1){
+                    ++nextSpaceIndex;
+                }
+                // ++oldStringIndex;
+            }
+
+            stringWithSpacesAdded.push_back(s[oldStringIndex]);
+        }
+
+        return stringWithSpacesAdded;
+    }
+};
