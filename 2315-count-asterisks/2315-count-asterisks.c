d4d5ffb1fa1f7384c7cf2048f188c0eb21655347
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2026-01-07 21:02:46 -0800

    Time: 3 ms (11.76%), Space: 7.8 MB (86.27%) - LeetHub

---
+++ 2315-count-asterisks/2315-count-asterisks.c
@@ -0,0 +1,20 @@
+int countAsterisks(const char* str) {
+    bool isBetweenVerticalBars = false;
+    int numberOfAsterisksInBetweenVerticalBars = 0;
+
+    for(int i = 0; i < strlen(str); ++i){
+        if(isBetweenVerticalBars == false && str[i] == '*'){
+            ++numberOfAsterisksInBetweenVerticalBars;
+        }
+        else if(str[i] == '|'){
+            if(isBetweenVerticalBars == false){
+                isBetweenVerticalBars = true;
+            }
+            else{
+                isBetweenVerticalBars = false;
+            }
+        }
+    }
+
+    return numberOfAsterisksInBetweenVerticalBars;     
+}
