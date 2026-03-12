2930662a18c3fa976288cc3ec971c3234cbe5ed7
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2026-03-11 21:32:02 -0700

    Time: 18 ms (71.84%), Space: 25.2 MB (62.78%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int maxDistinct(string str) {
         auto numberOfSubstringsWithDistinctFirstLetter {0};
-        
         std::unordered_map<char, bool> alreadyUsedLettersOfSubstrings{};
 
         for(const auto& letter : str){
