35ee0d6590283f2c0ebb0e435f8b0cc214efd11a
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2026-04-14 19:18:25 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    bool checkIfPangram(std::string_view sentence) {
-        std::set<int> allLettersInAlphabetDecimalValues {};
-
+    void addAlphabetDecimalValuesToSet(std::set<int>& allLettersInAlphabetDecimalValues){
         for(int i {97}; i <= 122; ++i){
             allLettersInAlphabetDecimalValues.insert(i);
         }
+    }
 
+    bool allLettersInAlphabetExistInSentence(std::string_view sentence, std::set<int>& allLettersInAlphabetDecimalValues){
         for(const auto& character : sentence){
             allLettersInAlphabetDecimalValues.erase(static_cast<int>(character));
             
@@ -14,6 +14,16 @@ public:
                 return true;
             }
         }
+
+        return false;
+    }
+
+    bool checkIfPangram(std::string_view sentence) {
+        std::set<int> allLettersInAlphabetDecimalValues {};
+
+        addAlphabetDecimalValuesToSet(allLettersInAlphabetDecimalValues);
+
+        return allLettersInAlphabetExistInSentence(sentence, allLettersInAlphabetDecimalValues);
 
         return false;
     }
