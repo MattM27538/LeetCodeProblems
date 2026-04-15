cb3c5c8b3568dcb973f4fdf329b478d032f3c497
MattM27538 <143589127+MattM27538@users.noreply.github.com>
2026-04-14 19:11:22 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     bool checkIfPangram(std::string_view sentence) {
-        std::set<int> allLettersInAlphabetDecimalValues {97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 
-        108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122};
+        std::set<int> allLettersInAlphabetDecimalValues {};
+
+        for(int i {97}; i <= 122; ++i){
+            allLettersInAlphabetDecimalValues.insert(i);
+        }
 
         for(const auto& character : sentence){
             allLettersInAlphabetDecimalValues.erase(static_cast<int>(character));
